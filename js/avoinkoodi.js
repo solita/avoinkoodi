74cf9d9dc8843460109d66540fbc953e75330742
Iiro Uusitalo <iiro.uusitalo@solita.fi>
2016-02-05 22:16:14 +0200

    Sorttaus toimintaan

--- js/avoinkoodi.js
+++ js/avoinkoodi.js
@@ -1,15 +1,25 @@
 $(document).ready(function() {
     $("#projects").html('<tr><td colspan="3">Ladataan sisältöä...</td></tr>');
     json_url = window.location.pathname;
+
     if (window.location.pathname.endsWith('/')) {
-	json_url = json_url.substring(0, json_url.length - 1);
+        json_url = json_url.substring(0, json_url.length - 1);
     }
+
     $.getJSON(json_url + "/projects.json", function(data) {
         $("#projects").html("");
+
+        data.projects.sort(function(a, b) {
+            ownerA = a.owner.toLowerCase();
+            ownerB = b.owner.toLowerCase();
+
+            return ownerA.localeCompare(ownerB);
+        });
+
         $.each(data.projects, function(index, row) {
             $("#projects").append("<tr><td>"+ row.owner + "</td><td>" + row.project + "</td><td><a href='" + row.code_url + "'>" + row.code_url + "</a></td>" +
-				  "<td><a href='" + row.service_url + "'>" + row.service_url + "</a></td>" +
-				  "</tr>");
+                    "<td><a href='" + row.service_url + "'>" + row.service_url + "</a></td>" +
+                    "</tr>");
         });
     });
 });
