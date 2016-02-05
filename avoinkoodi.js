b9cb90049ecd10150bc8c368ee9dcf89850d5ff5
Antti Virtanen <antti.virtanen@iki.fi>
2016-02-05 15:00:39 +0200

    Projektilista osaksi branchia ja mukaan lisätietokenttiä

--- avoinkoodi.js
+++ avoinkoodi.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
     $("#projects").html('<tr><td colspan="3">Ladataan sisältöä...</td></tr>');
-    $.getJSON("https://raw.githubusercontent.com/solita/avoinkoodi/master/projects.json", function(data) {
+    $.getJSON("/projects.json", function(data) {
         $("#projects").html("");
         $.each(data.projects, function(index, row) {
-            $("#projects").append("<tr><td>"+ row.owner + "</td><td>" + row.project + "</td><td><a href='" + row.code_url + "'>" + row.code_url + "</a></td></tr>");
+            $("#projects").append("<tr><td>"+ row.owner + "</td><td>" + row.project + "</td><td><a href='" + row.code_url + "'>" + row.code_url + "</a></td>" +
+				  "<td><a href='" + row.service_url + "'>" + row.service_url + "</a></td>" +
+				  "</tr>");
         });
     });
 });
