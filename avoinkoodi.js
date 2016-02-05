155d524e31bfbbef8c145ebc3b2a35b3bf94e549
Antti Virtanen <antti.virtanen@iki.fi>
2016-02-05 15:32:11 +0200

    JSON datan haku huomioi sen että polku ei ole välttämättä juuressa.

--- avoinkoodi.js
+++ avoinkoodi.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
     $("#projects").html('<tr><td colspan="3">Ladataan sisältöä...</td></tr>');
-    $.getJSON("/projects.json", function(data) {
+    json_url = window.location.pathname;
+    if (window.location.pathname.endsWith('/')) {
+	json_url = json_url.substring(0, json_url.length - 1);
+    }
+    $.getJSON(json_url + "/projects.json", function(data) {
         $("#projects").html("");
         $.each(data.projects, function(index, row) {
             $("#projects").append("<tr><td>"+ row.owner + "</td><td>" + row.project + "</td><td><a href='" + row.code_url + "'>" + row.code_url + "</a></td>" +
