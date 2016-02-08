b810899dbdd0a14e7bf084a2b08b91d6cf0985bc
Juhani Kumara <juhani.kumara@solita.fi>
2016-02-08 16:25:15 +0200

    Resolving merge conflicts in pull request #40

--- js/avoinkoodi.js
+++ js/avoinkoodi.js
@@ -1,6 +1,6 @@
 "use strict";
 $(document).ready(function() {
-    $("#projects").html('<tr><td colspan="3">Ladataan sisältöä...</td></tr>');
+    $("#projects").html('<tr><td colspan="4">Ladataan sisältöä...</td></tr>');
     var json_url = window.location.pathname;
 
     if (window.location.pathname.endsWith('/')) {
@@ -13,17 +13,27 @@ $(document).ready(function() {
         data.projects.sort(function(a, b) {
             var ownerA = a.owner.toLowerCase();
             var ownerB = b.owner.toLowerCase();
-
             return ownerA.localeCompare(ownerB);
         });
 
         $.each(data.projects, function(index, row) {
+            var codeUrl = '-',
+                serviceUrl = '-';
+
+            if (row.code_url.length > 1) {
+                codeUrl = "<a href='" + $("<td>").text(row.code_url).html() + "'>N&auml;yt&auml; l&auml;hdekoodi</a>";
+            }
+
+            if (row.service_url.length > 1) {
+                serviceUrl = "<a href='" + $("<td>").text(row.service_url).html() + "'>Siirry palveluun</a>";
+            }
+
             $("#projects").append("<tr>" +
-                    "<td>"+ $("<td>").text(row.owner).html() + "</td>" +
-                    "<td>" + $("<td>").text(row.project).html() + "</td>" +
-                    "<td><a href='" + $("<td>").text(row.code_url).html() + "'>" + $("<td>").text(row.code_url).html() + "</a></td>" +
-                    "<td><a href='" + $("<td>").text(row.service_url).html() + "'>" + $("<td>").text(row.service_url).html() + "</a></td>" +
-                    "</tr>");
+                "<td>"+ $("<td>").text(row.owner).html() + "</td>" +
+                "<td>" + $("<td>").text(row.project).html() + "</td>" +
+                "<td>" + codeUrl + "</td>" +
+                "<td>" + serviceUrl + "</td>" +
+                "</tr>");
         });
     });
 });
