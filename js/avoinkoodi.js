dd785374303bb34940ecf32061e9cef3b8c14c50
Juhani Kumara <juhani.kumara@solita.fi>
2016-03-08 14:44:20 +0200

    New styles by Heikki Niemi. Added Facebook and Google+ share counts.

--- js/avoinkoodi.js
+++ js/avoinkoodi.js
@@ -1,39 +1,58 @@
-"use strict";
 $(document).ready(function() {
-    $("#projects").html('<tr><td colspan="4">Ladataan sisältöä...</td></tr>');
-    var json_url = window.location.pathname;
 
-    if (window.location.pathname.endsWith('/')) {
-        json_url = json_url.substring(0, json_url.length - 1);
-    }
+	$('.menuicon').click(function() {
+	   $( '.menu-container' ).toggleClass('opened'); 
+	});
+	
+	$('li a').click(function(){
+	  $('html, body').animate({
+			scrollTop: $( $.attr(this, 'href') ).offset().top - 50
+	  }, 500); 
+  
+	  return false;
+	});
+	
+	/* Get FB shares */
 
-    $.getJSON(json_url + "/projects.json", function(data) {
-        $("#projects").html("");
+	$.get('http://graph.facebook.com/?id=http://www.avoinkoodi.fi',
+		function(data) {
+			if (data.shares) {
+				$('#fb-share-count').text(data.shares);			
+			}
+		}
+	);
 
-        data.projects.sort(function(a, b) {
-            var ownerA = a.owner.toLowerCase();
-            var ownerB = b.owner.toLowerCase();
-            return ownerA.localeCompare(ownerB);
-        });
+	/* Get G+ shares */
 
-        $.each(data.projects, function(index, row) {
-            var codeUrl = '-',
-                serviceUrl = '-';
+	$.ajax({
+		type: 'POST',
+		url: 'https://clients6.google.com/rpc',
+		contentType: 'application/json; charset=utf-8',
+		data: JSON.stringify([{
+	    'method': 'pos.plusones.get',
+	    'id': 'p',
+	    'params':{
+        'nolog': true,
+        'id': 'http://www.avoinkoodi.fi/',
+        'source': 'widget',
+        'userId': '@viewer',
+        'groupId': '@self'
+        },
+	    'jsonrpc': '2.0',
+	    'key': 'p',
+	    'apiVersion': 'v1'
+		}]),
+		success: function(data) {
+			var plusOnes = data[0].result.metadata.globalCounts.count;
+			if (plusOnes) {
+				$('#gplus-share-count').text(plusOnes);
+			}
+		}
+	});
 
-            if (row.code_url.length > 1) {
-                codeUrl = "<a href='" + $("<td>").text(row.code_url).html() + "'>N&auml;yt&auml; l&auml;hdekoodi</a>";
-            }
-
-            if (row.service_url.length > 1) {
-                serviceUrl = "<a href='" + $("<td>").text(row.service_url).html() + "'>Siirry palveluun</a>";
-            }
-
-            $("#projects").append("<tr>" +
-                "<td>"+ $("<td>").text(row.owner).html() + "</td>" +
-                "<td>" + $("<td>").text(row.project).html() + "</td>" +
-                "<td>" + codeUrl + "</td>" +
-                "<td>" + serviceUrl + "</td>" +
-                "</tr>");
-        });
-    });
+	$('.share-link').click(function(ev) {
+		window.open(this.href, 'share', 'left=20,top=20,width=500,height=500,toolbar=1,resizable=0');
+		ev.preventDefault();
+		return false;
+	});
 });
