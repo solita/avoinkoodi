27c9b431f9afe74edf8abb18cb42a2304154ebba
Juhani Kumara <juhani.kumara@solita.fi>
2016-03-09 15:59:19 +0200

    Fixed some texts. Cleaned up styles and removed obsolete files. Added package.json and node-sass for easy sass-building. Added .gitignore file. Project data is now fetched from the json again.

--- js/avoinkoodi.js
+++ js/avoinkoodi.js
@@ -1,18 +1,64 @@
-$(document).ready(function() {
+'use strict';
+
+(function ($) {
+  var $projects = $("#projects");
+  $projects.html("<tr><td colspan='4'>Ladataan sisältöä...</td></tr>");
+
+	// Load projects
+
+  $.getJSON("projects.json", function(data) {
+      $projects.html("");
+
+      data.projects.sort(function(a, b) {
+          var ownerA = a.owner.toLowerCase();
+          var ownerB = b.owner.toLowerCase();
+          return ownerA.localeCompare(ownerB);
+      });
+
+      var content = data.projects.map(function (project) {
+          var codeUrl = '-',
+              serviceUrl = '-';
+
+          if (project.code_url.length > 1) {
+              codeUrl = "<a href='" + $("<td>").text(project.code_url).html() + "'>Näytä lähdekoodi</a>";
+          }
+
+          if (project.service_url.length > 1) {
+              serviceUrl = "<a href='" + $("<td>").text(project.service_url).html() + "'>Siirry palveluun</a>";
+          }
+
+          return "<tr>" +
+                 "<td>" + $("<td>").text(project.owner).html() + "</td>" +
+                 "<td>" + $("<td>").text(project.project).html() + "</td>" +
+                 "<td>" + codeUrl + "</td>" +
+                 "<td>" + serviceUrl + "</td>" +
+                 "</tr>";
+      });
+
+      $projects.html(content.join(""));
+  });
+
+  // Register events
 
 	$('.menuicon').click(function() {
 	   $( '.menu-container' ).toggleClass('opened'); 
 	});
 	
-	$('li a').click(function(){
+	$('#mainmenu a').click(function(){
 	  $('html, body').animate({
 			scrollTop: $( $.attr(this, 'href') ).offset().top - 50
 	  }, 500); 
   
 	  return false;
 	});
-	
-	/* Get FB shares */
+
+	$('.share-link').click(function(ev) {
+		window.open(this.href, 'share', 'left=20,top=20,width=500,height=500,toolbar=1,resizable=0');
+		ev.preventDefault();
+		return false;
+	});
+
+	// Get FB shares
 
 	$.get('http://graph.facebook.com/?id=http://www.avoinkoodi.fi',
 		function(data) {
@@ -22,7 +68,7 @@ $(document).ready(function() {
 		}
 	);
 
-	/* Get G+ shares */
+	// Get G+ shares
 
 	$.ajax({
 		type: 'POST',
@@ -49,10 +95,4 @@ $(document).ready(function() {
 			}
 		}
 	});
-
-	$('.share-link').click(function(ev) {
-		window.open(this.href, 'share', 'left=20,top=20,width=500,height=500,toolbar=1,resizable=0');
-		ev.preventDefault();
-		return false;
-	});
-});
+})(jQuery);
